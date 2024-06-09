e8b8b711a0afccef7ce98ccb4ec9edc4c595c63b
KathCabsVA <155535379+KathCabsVA@users.noreply.github.com>
2024-06-09 23:31:25 +0800

    i did my best

--- Assets/DragAndShoot.cs
+++ Assets/DragAndShoot.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 
 public class DragAndShoot : MonoBehaviour
 {
+    public Transform launchPoint;
+    public GameObject projectile;
+    public float launchSpeed = 10f;
+
+    [Header("Trajectory Display")]
+    public LineRenderer lineRenderer;
+    public int linePoints = 175;
+    public float timeIntervalinPoints = 0.10f;
 
     private Vector3 mousePressDownPos;
     private Vector3 mouseReleasePos;
@@ -12,7 +20,7 @@ public class DragAndShoot : MonoBehaviour
     private Rigidbody rb;
 
     private bool isShoot;
- 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -20,6 +28,7 @@ public class DragAndShoot : MonoBehaviour
 
     private void OnMouseDown()
     {
+
         mousePressDownPos = Input.mousePosition;
     }
 
@@ -41,4 +50,35 @@ public class DragAndShoot : MonoBehaviour
         isShoot = true;
     }
 
-}
+    void Update()
+    {
+        if (lineRenderer != null)
+        {
+            if (Input.GetMouseButton(1))
+            {
+                DrawTrajectory();
+                lineRenderer.enabled = true;
+            }
+            else
+                lineRenderer.enabled = false;
+        }
+
+        void DrawTrajectory()
+        {
+            Vector3 origin = launchPoint.position;
+            Vector3 startVelocity = launchSpeed * launchPoint.up;
+            lineRenderer.positionCount = linePoints;
+            float time = 0;
+            for (int i = 0; i < linePoints; i++)
+            {
+                //s = u*t + 1/2*g*t*t
+                var x = (startVelocity.x * time) + (Physics.gravity.x / 2 * time * time);
+                var z = (startVelocity.z * time) + (Physics.gravity.z / 2 * time * time);
+                Vector3 point = new Vector3(x, 0, z);
+                lineRenderer.SetPosition(i, origin + point);
+                time += timeIntervalinPoints;
+            }
+        }
+    }
+} 
+
--- Assets/TrajectoryLine.cs
+++ Assets/TrajectoryLine.cs
@@ -40,8 +40,8 @@ public class TrajectoryLine : MonoBehaviour
         {
             //s = u*t + 1/2*g*t*t
             var x = (startVelocity.x * time) + (Physics.gravity.x / 2 * time * time);
-            var y = (startVelocity.y * time) + (Physics.gravity.y / 2 * time * time);
-            Vector3 point = new Vector3(x, y, 0);
+            var z = (startVelocity.z * time) + (Physics.gravity.z / 2 * time * time);
+            Vector3 point = new Vector3(x, 0, z);
             lineRenderer.SetPosition(i, origin + point);
             time += timeIntervalinPoints;
         }
